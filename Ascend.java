475cb18e082f8acc9bf19e8b456b6f7f81f046d3
adam11111111111111 <adamrieger116@gmail.com>
2024-11-14 20:46:56 +0200

    [commit message]

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,18 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int lim = Integer.parseInt(args[0]);
+		int a = (int) (Math.random() * lim);
+		int b = (int) (Math.random() * lim);
+		int c = (int) (Math.random() * lim);
+		int min, mid, max, temp;
+		System.out.println(a + " " + b + " " + c);
+		temp = Math.min(a, b);
+		min = Math.min(temp, c);
+		temp = Math.max(a, b);
+		max = Math.max(temp, c);
+		mid = (a + b + c) - (max + min);
+		System.out.println(min + " " + mid + " " + max);
+
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,11 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
-	public static void main(String[] args){
-		// Replace this comment with your code
+	public static void main(String[] args) {
+		int currentValue = Integer.parseInt(args[0]);
+		int rate = Integer.parseInt(args[1]);
+		int n = Integer.parseInt(args[2]);
+		int futureValue = (int) (currentValue * Math.pow((1 + (rate / 100.0)), n));
+		System.out.println("After " + n + " years, $" + currentValue +
+				" saved at " + rate + ".0% will yield $" + futureValue);
 	}
 }
